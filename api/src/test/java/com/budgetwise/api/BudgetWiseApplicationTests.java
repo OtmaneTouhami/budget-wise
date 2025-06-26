af4aa6f3e95350e6eeef57297280987a414ba75b
Otmane TOUHAMI <otmanetouhami01@gmail.com>
2025-06-26 19:48:54 +0100

    Initialize BudgetWise API project with Maven setup, application properties, and basic structure

---
+++ api/src/test/java/com/budgetwise/api/BudgetWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetwise.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
