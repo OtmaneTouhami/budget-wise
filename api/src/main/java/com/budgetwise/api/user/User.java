0f5756ce1d5b81d6610df808a2cf27dad4c8f591
Otmane TOUHAMI <otmanetouhami01@gmail.com>
2025-06-27 17:42:04 +0100

    feat: add User entity with attributes and relationships for budget management

---
+++ api/src/main/java/com/budgetwise/api/user/User.java
@@ -0,0 +1,96 @@
+package com.budgetwise.api.user;
+
+import com.budgetwise.api.budget.Budget;
+import com.budgetwise.api.category.Category;
+import com.budgetwise.api.recurringtransaction.RecurringTransaction;
+import com.budgetwise.api.transaction.Transaction;
+import com.budgetwise.api.transactiontemplate.TransactionTemplate;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import static org.hibernate.annotations.UuidGenerator.Style.TIME;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator(style = TIME)
+    @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(unique = true, nullable = false, length = 254)
+    private String email;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String username;
+
+    @Column(name = "first_name", nullable = false, length = 100)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 100)
+    private String lastName;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(name = "phone_number", nullable = false, length = 20)
+    private String phoneNumber;
+
+    @Builder.Default
+    @Column(name = "date_format", length = 10, columnDefinition = "VARCHAR(10) DEFAULT 'YYYY-MM-DD'")
+    private String dateFormat = "YYYY-MM-DD";
+
+    @Builder.Default
+    @Column(length = 3, columnDefinition = "VARCHAR(3) DEFAULT 'MAD'")
+    private String currency = "MAD";
+
+    @Builder.Default
+    @Column(name = "is_active", nullable = false, columnDefinition = "boolean DEFAULT true")
+    private boolean isActive = true;
+
+    @Builder.Default
+    @Column(name = "is_deleted", nullable = false, columnDefinition = "boolean DEFAULT false")
+    private boolean isDeleted = false;
+
+    private LocalDateTime lastLoginDate;
+    private LocalDateTime lastPasswordResetDate;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Category> categories;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Budget> budgets;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<TransactionTemplate> transactionTemplates;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<RecurringTransaction> recurringTransactions;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Transaction> transactions;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+}
